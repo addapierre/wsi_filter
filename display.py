a7c779935cb58fdf2f68c39a69de174959e39b83
addapierre <addapierr@gmail.com>
2023-12-12 10:04:25 +0100

    new improved architecture, functions slightly optimised

---
+++ display.py
@@ -0,0 +1,137 @@
+import cv2
+import numpy as np
+
+
+
+class DisplayImage():
+
+    def __init__(self, image) -> None:
+        self.current_image = image.copy() # currently displayed image
+        self.image_prezoom = self.current_image.copy()
+        self.height = image.shape[0]
+        self.width = image.shape[1]
+
+        self.zoom_triggered = 0
+        self.is_zoomed = 0
+
+        self.x_init_zoom = None
+        self.y_init_zoom = None
+        self.resize_ratio = None
+ 
+    def begin_zoom(self, x, y): 
+        self.image_prezoom = self.current_image.copy()
+        self.zoom_triggered = 1
+        self.x_init_zoom = x
+        self.y_init_zoom = y
+
+    def zoom_rectangle(self, x, y):
+        self.current_image = self.image_prezoom.copy()
+        cv2.rectangle(self.current_image, (self.x_init_zoom, self.y_init_zoom), (x,y), (255,0,0), 2)
+
+    def cancel_zoom(self):
+        self.zoom_triggered = 0
+        self.is_zoomed = 0
+        self.current_image = self.image_prezoom.copy()
+
+    def zoom(self, x, y):
+        #try:
+            self.current_image = self.image_prezoom.copy()
+            self.zoom_triggered = 0
+            self.is_zoomed = 1
+            self.x_final_zoom = x
+            self.y_final_zoom = y
+
+            x_zoom_1 = self.x_init_zoom ; x_zoom_2 = self.x_final_zoom
+            y_zoom_1 = self.y_init_zoom ; y_zoom_2 = self.y_final_zoom
+
+            # cropping
+            self.xlist = np.sort(np.array([x_zoom_1, x_zoom_2]))
+            self.ylist = np.sort(np.array([y_zoom_1, y_zoom_2]))
+            self.current_image = self.current_image[self.ylist[0] : self.ylist[1], self.xlist[0] : self.xlist[1]]
+
+            # resize so that the area covered by the image is the same
+            new_w = self.xlist[1] - self.xlist[0]
+            new_h = self.ylist[1] - self.ylist[0]
+            self.resize_ratio = np.sqrt(self.height*self.width/(new_h*new_w))
+            try:
+                new_size = np.array([int(new_w * self.resize_ratio), int(new_h * self.resize_ratio)])
+            except :
+                self.cancel_zoom()
+                return None
+            self.current_image = cv2.resize(self.current_image, new_size, interpolation = cv2.INTER_LANCZOS4)
+        # except: 
+        #     self.cancel_zoom()
+
+    def display_text(self, text, x, y, color = (255,0,0), font_multiplicator = 1e-3, thickness_multiplicator = 1e-3, background_color = None):
+        # Warning: this function changes self.current_image, make sure you made a copy before calling it
+        font_scale = min(self.width, self.height) * font_multiplicator
+        font_thickness = int(np.ceil(min(self.width, self.height) * thickness_multiplicator))
+        text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_DUPLEX, font_scale, font_thickness)
+        text_w, text_h = text_size
+        if x+text_w > self.current_image.shape[1]:
+            x = x-text_w
+        if y - text_h < 0:
+            y = y+text_h
+        if isinstance(background_color, tuple):
+            cv2.rectangle(self.current_image, (x, y-text_h-np.ceil(text_h/5).astype(int)), (x+text_w, y), background_color, -1)
+
+        cv2.putText(self.current_image, text, (x, y), cv2.FONT_HERSHEY_DUPLEX,\
+                    fontScale = font_scale, color = color, thickness = font_thickness, lineType = cv2.LINE_AA)
+        
+
+        
+def mouse_CB(event, x, y, flags, param):
+    """opencv mouse callback"""
+
+
+    if param.zoom_triggered:
+        if event == cv2.EVENT_LBUTTONUP:
+            param.zoom(x,y)
+
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if flags == 17: #shift pressed down
+                param.zoom_rectangle(x,y)
+            else:
+                param.cancel_zoom()
+
+    else:
+
+        if event == cv2.EVENT_MOUSEMOVE and param.labels is not None and param.is_wsi:
+            label = param.wsi_mask[y,x]
+            if label != 0 and not param.ratio_displayed:
+                param.display_ratio(label)
+                param.ratio_displayed = True
+            if label == 0 and param.ratio_displayed:
+                param.ratio_displayed = False
+                param.current_image = param.wsi.copy()
+
+        if event == cv2.EVENT_LBUTTONDOWN:
+            if (flags == 17) & (param.is_zoomed):
+                param.cancel_zoom()
+            elif flags == 17:
+                param.begin_zoom(x,y)
+            else:
+                pixel = param.current_image[y,x]
+                print('bgr : ',pixel)
+                hls_pixel = cv2.cvtColor(pixel[None, None, :], cv2.COLOR_BGR2HLS).squeeze()
+                print('hls : ',hls_pixel)
+                hsv_pixel = cv2.cvtColor(pixel[None, None, :], cv2.COLOR_BGR2HSV).squeeze()
+                print('hsv : ',hsv_pixel)
+                # if param.is_zoomed:
+                #     param.cancel_zoom()
+                # elif param.is_wsi:
+                #     label = param.wsi_mask[y,x]
+                #     param.open_label(label)
+        elif (event == cv2.EVENT_MBUTTONDOWN) | (event == cv2.EVENT_RBUTTONDOWN):
+
+            if param.is_zoomed:
+                param.cancel_zoom()
+            elif param.is_wsi:
+                if param.ratio_displayed:
+                    param.ratio_displayed = False
+                label = param.wsi_mask[y,x]
+                param.display_blob(label)
+
+
+if __name__ == "__main__":
+    print('ok')
---
+++ process_wsi.py
@@ -0,0 +1,322 @@
+import os
+import sys
+import cv2
+import re
+import time
+import numpy as np
+import subprocess
+from subprocess import Popen, PIPE, STDOUT
+from utils import filter_blue_white, filter_small_blobs, lum_contrast, filter_small_holes, \
+                    separate_purple_orange
+
+from display import DisplayImage, mouse_CB
+
+class ImageNDPI(DisplayImage):
+    def __init__(self, ndpi_path):
+
+        assert os.path.exists(ndpi_path), f"ndpi file {ndpi_path} not found"
+        assert ndpi_path.split('.')[-1] == 'ndpi', f'{ndpi_path} is not a .ndpi file'
+
+        self.ndpi_path = ndpi_path
+        self.mag_list = np.array([40, 20, 10, 5, 2.5, 1.25])
+        self.mag_max, self.max_resolution = self.get_header_info()
+        if self.mag_max == 20:
+            self.mag_list = self.mag_list[1:]
+        
+
+        # get magnification closest to 1
+        self.current_mag = self.wsi_mag = 1.25
+        self.current_resolution = self.wsi_resolution = self.max_resolution * self.current_mag / self.mag_max
+        self.raw_wsi = self.ndpi_split(self.current_mag, 0, 0, 1, 1, normalized=True)
+        super().__init__(self.raw_wsi.copy()) # current_image = wsi, raw_wsi remains unchanged
+        self.wsi_width = self.raw_wsi.shape[1] ; self.wsi_height = self.raw_wsi.shape[0]
+
+        self.blob_thresh = 0.1 # everything smaller in mm2 gets filtered
+        self.is_wsi = 1
+        self.labels = None
+        self.wsi_mask = np.zeros(self.raw_wsi.shape[:2])
+        self.mask = None
+        self.ratios = []
+        self.areas = []
+        self.ratio_displayed = False
+        self.wsi = None
+
+    def process_wsi(self, threshold = 0.1):
+        """
+        removes blue, white, small blobs and black blobs, creates a wsi_mask with a value/label per blob
+        Params: 
+            - img: RGB image to preprocess
+            - threshold: in mm2, blobs smaller than threshold are removed from the image. default: 0.2 mm2
+        Return: 
+            - preprocessed image
+            - mask with only blobs larger than threshold. blob values in mask correspond to their label
+        """
+        if not self.is_wsi:
+            return None
+
+        if threshold is None:
+            threshold = self.blob_thresh
+
+        self.current_image, _ = filter_blue_white(self.current_image) 
+
+        self.wsi_mask = filter_small_blobs(self.current_image.copy(), thresh = threshold, resolution = self.current_resolution)
+
+        #mask the image with 255 values
+        tt = np.dstack((np.clip(self.wsi_mask.copy(), 0, 1),)*3)
+        self.current_image = (self.current_image * tt) + (tt-1) 
+
+        self.labels = np.unique(self.wsi_mask)[1:]
+        # self.display_labels() # too slow
+        self.wsi = self.current_image.copy()
+
+
+    def open_label(self, label):
+        if (label == 0) or not self.is_wsi:
+            return None
+        #print(f"opening label {label}...")
+        y,x = np.where(self.wsi_mask == label)
+        xmin = x.min() ; ymin = y.min()
+        xmax = x.max() ; ymax = y.max()
+        
+        #select new mag and new resolution based on target image size
+        size_init = (xmax - xmin)*(ymax-ymin)/1e6 # target image size in Megapixel
+        target_size = size_init * np.square(self.mag_list/self.wsi_mag)
+        self.current_mag = self.mag_list[np.where(target_size<70)].max() # target image must be less than 70Mpix
+        self.current_resolution = self.max_resolution * self.current_mag / self.mag_max
+
+        #read cropped image
+        
+        self.current_image = self.ndpi_split(self.current_mag, xmin, ymin, xmax, ymax, normalized=False )
+        
+        # mask label img with cropped wsi mask
+        self.mask = self.wsi_mask[ymin:ymax, xmin:xmax].copy()
+        test = self.mask!=label
+        self.mask[test] = 0
+        self.mask[~test] = 1
+        self.mask = cv2.resize(self.mask, (self.current_image.shape[1], self.current_image.shape[0]))
+
+        #complete mask
+        
+        self.mask = filter_small_holes(self.mask, resolution=self.current_resolution) # mask = 0 on background and 1 on blob
+        mask_3ch = np.dstack((self.mask,)*3)
+        self.current_image = self.current_image * mask_3ch + (mask_3ch-1)
+        self.current_image, self.mask = filter_blue_white(self.current_image)
+        self.current_image = self.current_image + np.dstack((self.mask-1,)*3) #replace 0 with 255
+
+    def process_label(self, keep_images = True):
+        "returns the blob's area in mm2 and the ratio of fibrosis orange/(orange+purple). can create separated self.orange and self.purple image if keep_image = True"
+        # if self.is_wsi:
+        #     return None
+        # calculate blob area
+        nb_pixel = self.mask.sum()
+        pix_area = 1/(self.current_resolution[0]*self.current_resolution[1]) #mm2/pix
+        area = nb_pixel*pix_area
+
+        # orange ratio
+        ratio, self.purple, self.orange = separate_purple_orange(self.current_image, self.mask, keep_images=keep_images)
+        if keep_images:
+            self.original_image = self.current_image
+        return area, ratio
+    
+
+        # orange ratio
+        ratio, self.purple, self.orange = separate_purple_orange(self.current_image, self.mask, keep_images=keep_images)
+
+    def display_blob(self, label):
+        self.open_label(label)
+        self.is_wsi = 0
+        area, ratio = self.process_label(keep_images=True)
+        self.display_text(str(round(ratio, 2))+"%", 0, self.current_image.shape[0], font_multiplicator=0.005, thickness_multiplicator=0.008)
+    
+    def process_all_labels(self):
+        if self.labels is None:
+            return None
+        self.ratios = []
+        self.areas = []
+        for label in self.labels:
+            self.open_label(label)
+            area, ratio = self.process_label(keep_images=False)
+            # print(f"area : {round(area, 3)} mm2 |  ratio :  {round(ratio, 2)}%")
+            self.ratios.append(ratio)
+            self.areas.append(area)
+            #reinit
+            self.reset()
+        self.ratios = np.array(self.ratios)
+        self.areas = np.array(self.areas)
+        full_area = self.areas.sum()
+        ratio = (self.ratios*self.areas/full_area).sum()
+        self.display_text(str(round(ratio, 2))+"%", 0, self.current_image.shape[0], font_multiplicator=0.005, thickness_multiplicator=0.004)
+        self.wsi = self.current_image.copy()
+
+    def display_label(self, label):
+        """
+        displays blob label on the wsi 
+        """
+        
+        if self.labels is None or not self.is_wsi:
+            return None 
+                   
+        y,x = np.where(self.wsi_mask == label)
+        
+        xmin = x.min() ; ymin = y.min()
+        xmax = x.max() ; ymax = y.max()
+        x = int((xmax+xmin)/2)-40
+        y = int((ymax+ymin)/2)+40
+        
+        self.display_text(text=str(label), x=x, y=y)
+        self.wsi = self.current_image.copy()
+        
+        
+
+
+
+    def display_ratio(self, label):
+        if (len(self.ratios) == 0) | (self.ratio_displayed):
+            return None
+        i = np.where(self.labels == label)[0][0]
+        y,x = np.where(self.wsi_mask == label)
+        xmax = x.max()
+        ymin = y.min()
+        ratio = self.ratios[i]
+        area = self.areas[i]
+        text = f"{round(ratio, 1)}% , {round(area, 2)} mm2"
+        self.display_text(text, xmax, ymin, background_color=(200,200,200))
+
+
+    def reset(self, full_reset = False):
+        if full_reset:
+            self.current_image = self.raw_wsi.copy()
+            
+        else:
+            self.current_image = self.wsi.copy()
+        self.mask = self.wsi_mask 
+        self.current_mag = self.wsi_mag
+        self.current_resolution = self.wsi_resolution
+        self.is_wsi = 1
+        self.is_zoomed = 0
+        self.zoom_triggered = 0
+        self.ratio_displayed = False
+
+    def get_header_info(self):
+        """
+        reads the ndpi header, returns max magnification and max resolution in pix/mm
+        """
+        cmd = ['tifftopnm', '-headerdump', self.ndpi_path]
+        process = Popen(cmd, stdout=PIPE, stderr=STDOUT)
+        header = process.communicate()[0].decode('ISO-8859-1')
+
+        # Get Magnifications
+        pattern = r'Tag 65421: (\d+)'
+        _ =re.findall(pattern, header)
+        mag_max = int(_[0])
+
+        #Get resolution at mag_max
+        pattern = r'Resolution: (\d+), (\d+) pixels/cm'
+        numbersStrings = re.findall(pattern, header)
+        max_resolution = np.array([int(n) for tup in numbersStrings for n in tup]) / 10 # in pix/mm for maximum resolution
+        return mag_max, max_resolution
+
+    def ndpi_split(self, mag, xmin, ymin, xmax, ymax, normalized = True):
+        """ 
+        coordinates are tranformed in this method form xyxy into xywh
+        if normalized = True, coordinates are already normalized when passed to this method"""
+        if not normalized:
+            xmin = xmin / self.wsi_width  ; xmax = xmax / self.wsi_width
+            ymin = ymin / self.wsi_height ; ymax = ymax / self.wsi_height
+        if round(mag) == mag:
+            mag = str(round(mag))
+        else:
+            mag = str(round(mag,2))
+        cmd = ['ndpisplit', f'-x{mag}' ,f'-e{xmin},{ymin},{xmax-xmin},{ymax-ymin}', self.ndpi_path]
+        subprocess.call(cmd)
+        path = self.ndpi_path.replace('.ndpi', f'_x{mag}_z0_1.tif')
+        im = cv2.imread(path) 
+        os.remove(path)
+
+        return im
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    Image = ImageNDPI('data/19AA07204 03 HES.ndpi')
+
+    cv2.namedWindow('test-window', cv2.WINDOW_NORMAL)
+    cv2.setMouseCallback('test-window', mouse_CB, Image)    
+
+    while True:
+        # Image.process_wsi()
+        # Image.process_all_labels()
+        # sys.exit()
+
+
+        cv2.imshow('test-window', Image.current_image)
+        if (tt := cv2.waitKey(10) & int(0xFF)) != 255:
+            if tt == ord('q'):
+                sys.exit()
+            if tt == ord('c'):
+                Image.current_image = lum_contrast(Image.current_image)
+            if tt == ord('w'):
+                Image.process_wsi()
+            if tt == ord('x'):
+                start = time.time()
+                if Image.labels is None:
+                    Image.process_wsi()
+                    cv2.imshow('test-window', Image.current_image)
+                    cv2.waitKey(10)
+                Image.process_all_labels()
+                print(time.time()-start)
+            if tt == ord('r'):
+                Image.reset()
+            if tt == ord('f'):
+                Image.reset(full_reset=True)
+
+            if tt == ord('i'):
+                if not Image.is_wsi and not Image.is_zoomed:
+                    Image.current_image = Image.original_image
+                elif not Image.is_wsi and Image.is_zoomed:
+                    Image.image_prezoom = Image.original_image
+                    Image.current_image = Image.original_image
+                    Image.zoom(Image.x_final_zoom, Image.y_final_zoom)
+            if tt == ord('p'):
+                if not Image.is_wsi and not Image.is_zoomed:
+                    Image.current_image = Image.purple
+                elif not Image.is_wsi and Image.is_zoomed:
+                    Image.image_prezoom = Image.purple
+                    Image.current_image = Image.purple
+                    Image.zoom(Image.x_final_zoom, Image.y_final_zoom)
+            if tt == ord('o'):
+                if not Image.is_wsi and not Image.is_zoomed:
+                    Image.current_image = Image.orange
+                elif not Image.is_wsi and Image.is_zoomed:
+                    Image.image_prezoom = Image.orange
+                    Image.current_image = Image.orange
+                    Image.zoom(Image.x_final_zoom, Image.y_final_zoom)
+
+            if tt == ord('d'):
+                if Image.labels is None:
+                    Image.process_wsi()
+                    cv2.imshow('test-window', Image.current_image)
+                    cv2.waitKey(1)
+                
+                Image.ratios = []
+                Image.areas = []
+                for label in Image.labels:
+                    Image.open_label(label)
+                    area, ratio = Image.process_label(keep_images=False)
+                    Image.ratios.append(ratio)
+                    Image.areas.append(area)
+                    #reinit
+                    Image.reset()
+                    Image.display_label(label)
+                    cv2.imshow('test-window', Image.current_image)
+                    cv2.waitKey(1)
+                Image.ratios = np.array(Image.ratios)
+                Image.areas = np.array(Image.areas)
+                full_area = Image.areas.sum()
+                ratio = (Image.ratios*Image.areas/full_area).sum()
+                Image.display_text(str(round(ratio, 2))+"%", 0, Image.current_image.shape[0], font_multiplicator=0.005, thickness_multiplicator=0.004)
+                Image.wsi = Image.current_image.copy()
